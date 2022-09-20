cc8d62628773c395e42df5a3e46355ef60f7515d
Rebecca Mead <beckyleemead@gmail.com>
2022-09-20 18:32:30 -0400

    more fun in the exploration script

--- explore_data.py
+++ explore_data.py
@@ -1,11 +1,23 @@
 import tree
 
-data = ['a','b','c','d','e','f']
+characterData = ['a','b','c','d','e','f']
 
-t = tree.Tree()
+characterTree = tree.Tree()
 
-for c in data:
+for c in characterData:
     n = tree.Node(c)
-    t.insert(n)
+    characterTree.insert(n)
 
-t.breadth()
+characterTree.preorder(characterTree.head)
+print(characterTree.treeSum(characterTree.head))
+
+numberData = [0,1,2,3,4,5]
+
+numberTree = tree.Tree()
+
+for i in numberData:
+    n = tree.Node(i)
+    numberTree.insert(n)
+
+numberTree.breadth(True)
+print(numberTree.treeSum(numberTree.head))
